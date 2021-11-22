2666c5b6a1e76a03d771f1c546f6bd12fac93b2b
Ashutosh Malviya <ashutoshmalviya1000@gmail.com>
2021-11-22 09:22:31 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>Hello ! </h1>
+<EasyChatApp />
+</template>
+
+<script>
+import EasyChatApp from '@/components/EasyChatApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EasyChatApp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EasyChatApp.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Easy Chat App </h1>
+</template>
+
+<script>
+export default {
+  name: 'EasyChatApp',
+};
+</script>
